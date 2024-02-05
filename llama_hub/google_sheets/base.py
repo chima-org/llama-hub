653373fc0614f61d0df599f6eac4455fb85d728b
Anchit Bhatia <anchitbhatia23@gmail.com>
2024-02-05 20:44:53 +0000

    Updated google sheets to take credentials as parameter

--- llama_hub/google_sheets/base.py
+++ llama_hub/google_sheets/base.py
@@ -33,8 +33,9 @@ class GoogleSheetsReader(BaseReader):
 
     """
 
-    def __init__(self) -> None:
+    def __init__(self, credentials: dict) -> None:
         """Initialize with parameters."""
+        self.credentials = credentials
         try:
             import google  # noqa: F401
             import google_auth_oauthlib  # noqa: F401
@@ -162,26 +163,9 @@ class GoogleSheetsReader(BaseReader):
         Returns:
             Credentials, the obtained credential.
         """
-        from google.auth.transport.requests import Request
-        from google.oauth2.credentials import Credentials
-        from google_auth_oauthlib.flow import InstalledAppFlow
+        from google.oauth2.service_account import Credentials
 
-        creds = None
-        if os.path.exists("token.json"):
-            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
-                )
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open("token.json", "w") as token:
-                token.write(creds.to_json())
-
+        creds = Credentials.from_service_account_info(self.credentials)
         return creds
 
 
